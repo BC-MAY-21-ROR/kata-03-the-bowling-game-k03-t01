7ea82008ba07a889dae6b582e8de343c1dd38acf
julio cabrera <juliocabrera820@gmail.com>
2021-06-18 15:46:44 -0500

    test spare method
    Co-authored-by: Jairo Olan <olan.mayo05@gmail.com>

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,9 +1,9 @@
 require_relative '../src/game'
 
 describe Game do
-  let (:easy_scenario) { '14,45,6/,5/,x ,01,7/,6/,x ,2/6' }
-  let (:game) { Game.new(10)}
-  
+  let(:easy_scenario) { '14,45,6/,5/,x ,01,7/,6/,x ,2/6' }
+  let(:game) { Game.new(10) }
+
   it 'exists' do
     expect(Game).to be_a Class
   end
