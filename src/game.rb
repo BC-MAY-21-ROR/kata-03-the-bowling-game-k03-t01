24289b6c3e54f6205aa182f5bb12992fe6af5ab5
julio cabrera <juliocabrera820@gmail.com>
2021-06-18 21:12:57 -0500

    refactor: create new methods

--- src/game.rb
+++ src/game.rb
@@ -1,18 +1,23 @@
-class Game
-  attr_accessor :frames, :final_score
+require_relative './screen'
+require_relative './bonus'
 
-  def initialize(frames)
-    @frames = frames
+class Game
+  attr_accessor :plain_frames, :frames, :score, :final_score, :scores
+  attr_reader :play_type, :bonus
+
+  def initialize(plain_frames)
+    @plain_frames = plain_frames
+    @frames = []
     @score = 0
     @final_score = 0
     @scores = []
+    @play_type = {
+      spare: '/',
+      strike: 'X',
+      open: '-'
+    }.freeze
+    @bonus = Bonus.new
   end
-
-  PLAY_TYPE = {
-    spare: '/',
-    strike: 'X',
-    open: '-'
-  }.freeze
 
   def start
     frames.each do |index, frame|
@@ -21,11 +26,11 @@ class Game
         second_try = 0
         first_try = frame.first if open?(frame.first)
         second_try = frame.second if open?(frame.second)
-        @score = first_try + second_try
-        scores[index] = @score
+        score = first_try + second_try
+        scores[index] = score
       elsif spare?(frame.second)
-        @score = 10 + bonus_spare(index)
-        scores[index] = @score
+        score = 10 + bonus.spare(frames, index)
+        scores[index] = score
       end
     end
   end
@@ -34,19 +39,19 @@ class Game
   def tenth_frame; end
 
   def strike?(frame)
-    frame.first == PLAY_TYPE[:strike]
+    frame.first == play_type[:strike]
   end
 
   def spare?(try)
-    try == PLAY_TYPE[:spare]
+    try == play_type[:spare]
   end
 
   def open?(try)
-    try == PLAY_TYPE[:open]
+    try == play_type[:open]
   end
 
   def frame_open?(frame)
-    frame.first == PLAY_TYPE[:open] || frame.second == PLAY_TYPE[:open]
+    frame.first == play_type[:open] || frame.second == play_type[:open]
   end
 
   # TODO: create the scenario when the first try is a strike
@@ -57,16 +62,22 @@ class Game
 
   # TODO: refactor method by using small methods
   def show_frames
-    input_frames = '14,45,6/,5/,x ,-1,7/,6/,x ,2/6'.split(',')
-    9.times {|index| input_frames[index] = [input_frames[index][0], input_frames[index][1]] } 
-    input_frames[9] = [input_frames[9][0], input_frames[9][1], input_frames[9][2]]
-    input_frames.each do |frame|
-      print "#{frame} "
-    end
-    puts
-    9.times do
-      print "    [#{9}]    "
-    end
-    print "      [#{9}]      "
+    Screen.new.show_frames(frames, scores)
+  end
+
+  def fill_frames
+    frame_array = plain_frames.split(',')
+    @frames = split_frames(frame_array)
+  end
+
+  def split_frames(frame_array)
+    splitted_frames = Array.new(10)
+    9.times { |index| splitted_frames[index] = [frame_array[index][0], frame_array[index][1]] }
+    splitted_frames[9] = [frame_array[9][0], frame_array[9][1], frame_array[9][2]]
+    splitted_frames
   end
 end
+
+game = Game.new('14,45,61,52,41,-1,72,61,51,236')
+game.fill_frames
+game.show_frames
