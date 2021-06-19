58c2113d4cd9eea5507f88ee954e1150c6cfbaba
julio cabrera <juliocabrera820@gmail.com>
2021-06-18 21:14:27 -0500

    create test body

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,14 +1,20 @@
 require_relative '../src/game'
 
 describe Game do
-  let(:easy_scenario) { '14,45,6/,5/,x ,01,7/,6/,x ,2/6' }
+  let(:easy_scenario) { '14,45,61,52,41,-1,72,61,51,236' }
+  let(:base_scenario) { '14,45,6/,5/,x ,01,7/,6/,x ,2/6' }
   let(:game) { Game.new(10) }
 
   it 'exists' do
     expect(Game).to be_a Class
   end
 
-  it 'can tell is a spare' do
-    expect(game.spare?('/')).to be true
+  xit 'returns a spare' do
+  end
+
+  xit 'returns a strike' do
+  end
+
+  xit 'returns a open' do
   end
 end
