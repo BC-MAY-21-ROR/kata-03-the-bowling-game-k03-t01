7a8fa3618a03416340c9e387a285839fc9c6986e
julio cabrera <juliocabrera820@gmail.com>
2021-06-18 21:13:16 -0500

    add type of bonus

--- src/bonus.rb
+++ src/bonus.rb
@@ -1,7 +1,11 @@
 class Bonus
-  def spare; end
-
-  def strike
-    bonus = fisrt_try + second_try
+  def spare(_frames, _frame_index)
+    next_try
   end
+
+  def strike(_frames, _frame_index)
+    fisrt_try + second_try
+  end
+
+  def next_try; end
 end
